ec57c350b58f6d67d0778bdff581c34947430178
Zebulun16 <palamoorzebulun@gmail.com>
2026-09-13 19:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (42.33%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.c
@@ -0,0 +1,32 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** transpose(int** matrix, int matrixSize, int* matrixColSize, int* returnSize, int** returnColumnSizes) {
+    int **result = malloc((*matrixColSize) * sizeof(int *));
+
+    for(int i = 0; i < *matrixColSize; i++)
+    {
+        result[i] = malloc(matrixSize * sizeof(int));
+    }
+
+    for(int i = 0; i < matrixSize; i++)
+    {
+        for(int j = 0; j < *matrixColSize; j++)
+        {
+            result[j][i] = matrix[i][j];
+        }
+    }
+
+    *returnSize = *matrixColSize;
+
+    *returnColumnSizes = malloc((*matrixColSize) * sizeof(int));
+
+    for(int i = 0; i < *matrixColSize; i++)
+    {
+        (*returnColumnSizes)[i] = matrixSize;
+    }
+
+    return result;
+}
